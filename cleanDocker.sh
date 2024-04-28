f219c44a7846a29975791edd0e59a684174215bb
CoMarBoys <macceluno@hotmail.com>
2024-04-28 22:27:44 +0200

    Create cleanDocker.sh

---
+++ cleanDocker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Display list of all Docker contaimers and images before dele#tion
+echo "Before Deletion:"
+docker ps -a
+docker images
+
+#Stop all running containers
+docker stop $(docker ps -a -q)
+
+#Remove all stopped containers
+docker rm $(docker ps -a -q)
+
+#Prune volumes not used by any container
+docker volume prune -f
+
+#Prune all unused images
+docker image prune -a -f
+
+# Prune Docker system(included stopped containers, networks,dangling images and optionally unused volumes
+docker system prune -a -f
+
+echo "After deletion"
